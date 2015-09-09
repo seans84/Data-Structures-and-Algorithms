e0457b351391d10593d85b79cdf1510b55ca74d5
Sean Szurko <sean_szurko@yahoo.com>
2015-09-09 03:32:26 -0700

    Binary tree start

--- BinaryTree.rb
+++ BinaryTree.rb
@@ -1,2 +1,13 @@
 class Node
+  attr_accessor :value, :parent, :left_child, :right_child
+  
+  def initialize(value)
+    @value = value
+    @left_child = 
+    @right_child = 
+  end
+  
+  def build_tree(arr)
+    
+  end
 end
